617f651da33e1b499a5ef84d30f30eeeea0d8f5e
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2025-07-27 01:57:21 +0600

    NewsLetter Footer Allrooms

--- Clint/src/App.jsx
+++ Clint/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
+import AllRooms from "./Pages/AllRooms";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,8 +14,10 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 };
--- Clint/src/Pages/Home.jsx
+++ Clint/src/Pages/Home.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import FeatureDestination from '../Components/FeatureDestination';
-import ExclusiveOffers from '../Components/ExclusiveOffers';
-import Testimonial from '../Components/Testimonial';
+import React from "react";
+import Hero from "../Components/Hero";
+import FeatureDestination from "../Components/FeatureDestination";
+import ExclusiveOffers from "../Components/ExclusiveOffers";
+import Testimonial from "../Components/Testimonial";
+import NewsLetter from "../Components/NewsLetter";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <FeatureDestination/>
-      <ExclusiveOffers/>
-      <Testimonial/>
+      <FeatureDestination />
+      <ExclusiveOffers />
+      <Testimonial />
+      <NewsLetter />
     </>
   );
-}
+};
 
-export default Home
+export default Home;

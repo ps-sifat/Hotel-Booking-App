646fc99337c587dce8967e455f1bd03cf0e2872b
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2025-07-28 20:39:36 +0600

    some modify myBooking page

--- Clint/src/Pages/MyBooking.jsx
+++ Clint/src/Pages/MyBooking.jsx
@@ -55,9 +55,42 @@ const MyBooking = () => {
               </div>
             </div>
             {/* date & time  */}
-            <div></div>
+            <div className="flex flex-row md:items-center md:gap-12 mt-3 gap-8">
+              <div>
+                <p>Check-In:</p>
+                <p className="text-gray-500 text-sm">
+                  {new Date(booking.checkInDate).toDateString()}
+                </p>
+              </div>
+              <div>
+                <p>Check-out:</p>
+                <p className="text-gray-500 text-sm">
+                  {new Date(booking.checkOutDate).toDateString()}
+                </p>
+              </div>
+            </div>
             {/* pryment  */}
-            <div></div>
+            <div className="flex flex-col items-start justify-center pt-3">
+              <div className="flex items-center gap-2">
+                <div
+                  className={`h-3 w-3 rounded-full ${
+                    booking.isPaid ? "bg-green-500" : "bg-red-500"
+                  }`}
+                ></div>
+                <p
+                  className={`text-sm ${
+                    booking.isPaid ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  {booking.isPaid ? "Paid" : "Unpaid"}{" "}
+                </p>
+              </div>
+              {!booking.isPaid && (
+                <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-200 hover:-translate-y-1 transition-all cursor-pointer ease-in ">
+                  Pay Now
+                </button>
+              )}
+            </div>
           </div>
         ))}
       </div>

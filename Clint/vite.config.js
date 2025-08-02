bf2eb0706b27ddbcb6c41542770d7e4b2a13a29d
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2025-08-02 18:32:03 +0600

    for vercel

--- Clint/vite.config.js
+++ Clint/vite.config.js
@@ -2,5 +2,4 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Hotel-Booking-App",
 });

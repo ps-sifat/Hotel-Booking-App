8a1a563a44a2daaa965e379c43a055c4c3c62c57
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2025-08-02 18:40:00 +0600

    for vercel app

--- Clint/vite.config.js
+++ Clint/vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  builds: [{ src: "index.html", use: "@vercel/static" }],
+  routes: [{ src: "/(.*)", dest: "/index.html" }],
 });
